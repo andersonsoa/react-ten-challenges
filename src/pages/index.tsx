4dab5a8f4988b56595d9d73d2bb25bb321d1d71c
Anderson Soares <anderson.ceu@gmail.com>
2022-01-05 16:45:53 -0400

    add chakra ui

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@chakra-ui/react";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <Box p="20">
+      <Text color="pink.600">Hello</Text>
+    </Box>
+  );
+};
+
+export default Home;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    body: "'Poppins', sans-serif",
+    heading: "'Poppins', serif",
+    mono: "Menlo, monospace",
+  },
+});
